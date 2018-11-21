10e848355a2679912abc281878d208e882c9d9b3
simon <simon@VMSimon.VMDOM.com>
2018-11-21 15:26:21 +0100

    test jenkins

---
+++ ODL_REPOSITORY/odl_dao_core/src/test/java/com/douillet/odl_dao_core/UtilisateurDaoTest.java
@@ -0,0 +1,34 @@
+package com.douillet.odl_dao_core;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class UtilisateurDaoTest {
+	 /*
+	   private static PlatformTransactionManager transactionManager;
+	 
+	   @BeforeClass
+	   public static void setUpBeforeClass() throws Exception {
+	  	     transactionManager = (PlatformTransactionManager) context.getBean("txManager");
+	   }
+	   
+	   @Before
+	   public void setUp() {
+	     status = transactionManager.getTransaction(null);
+	     logger.info("Session et transaction initialisés");
+	   }
+	   
+	   @After
+	   public void tearDown() {
+	     transactionManager.commit(status);
+	   }
+   */
+
+	 }
