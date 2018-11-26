8f05adf72e322489c8fe8d0b8b233994560468e9
simon <simon@VMSimon.VMDOM.com>
2018-11-26 17:01:20 +0100

    SDT - ajout des tests unitaires

--- ODL_REPOSITORY/odl_dao_core/src/test/java/com/douillet/odl_dao_core/UtilisateurDaoTest.java
+++ ODL_REPOSITORY/odl_dao_core/src/test/java/com/douillet/odl_dao_core/UtilisateurDaoTest.java
@@ -1,34 +1,53 @@
 package com.douillet.odl_dao_core;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
-import org.springframework.transaction.PlatformTransactionManager;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.util.List;
 
-public class UtilisateurDaoTest {
-	 /*//::
-	   private static PlatformTransactionManager transactionManager;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import com.douillet.odl_dao_api.UtilisateurDAO;
+
+import odl_hibernate_model.Adresse;
+import odl_hibernate_model.Utilisateur;
+
+@ContextConfiguration(classes = com.douillet.odl_config.contextConfig.class )
+@RunWith(SpringJUnit4ClassRunner.class)
+public class UtilisateurDaoTest  {
+
+ 
+	    @Autowired
+	    private UtilisateurDAO UtilisateurDAO;
 	 
-	   @BeforeClass
-	   public static void setUpBeforeClass() throws Exception {
-	  	     transactionManager = (PlatformTransactionManager) context.getBean("txManager");
-	   }
-	   
-	   @Before
-	   public void setUp() {
-	     status = transactionManager.getTransaction(null);
-	     logger.info("Session et transaction initialisés");
-	   }
-	   
-	   @After
-	   public void tearDown() {
-	     transactionManager.commit(status);
-	   }
-   */
-
+	     
+	    @Test
+	    @Transactional
+	    @Rollback(true)
+	    public void testAddutilisateur()
+	    {
+	    	Utilisateur user = new Utilisateur();
+	    	Adresse adr = new Adresse();
+	    	
+	    	adr.setVille("Laval");
+	    	user.setLogin("test");
+	    	user.setPassword("test");
+	    	user.setAdresse(adr);
+	    	
+	    	UtilisateurDAO.addUtilisateur(user);
+	    	
+	    	
+	    	Utilisateur userinbase = UtilisateurDAO.getUtilisateur(user.getUserID());
+	    	    	 
+	    	Assert.assertSame(user.getLogin(),userinbase.getLogin());
+	    	Assert.assertSame(user.getAdresse(),userinbase.getAdresse());
+	        
+	    }
+	    
 	 }
