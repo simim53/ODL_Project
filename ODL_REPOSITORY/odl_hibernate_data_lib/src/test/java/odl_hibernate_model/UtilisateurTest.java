0e2edbfd5bb3612e5a704fdb0625a41085480062
simon <simon@VMSimon.VMDOM.com>
2018-12-09 15:01:39 +0100

    correction test utilisateur model

--- ODL_REPOSITORY/odl_hibernate_data_lib/src/test/java/odl_hibernate_model/UtilisateurTest.java
+++ ODL_REPOSITORY/odl_hibernate_data_lib/src/test/java/odl_hibernate_model/UtilisateurTest.java
@@ -22,71 +22,34 @@ public class UtilisateurTest {
 	private String datelogin = "01/01/1990 - 22h22:22";
 	private Adresse adresse = new Adresse();
 	private Utilisateur user = new Utilisateur();
+	private int adresseid = 1;
 
 	@Test
-	public void getAdresse() {
-		adresse.setAdresseID(1);
+	public void createUtilisateur() {
+				
+		adresse.setAdresseID(adresseid);
 		user.setAdresse(adresse);
-		Assert.assertSame(user.getAdresse().getAdresseID(), 1);
-	}
-	@Test
-	public void getAge() {
 		user.setAge(age);
-		Assert.assertSame(user.getAge(), age);
-	}
-
-	@Test
-	public void getDateCreateAccount() {
 		user.setDateCreateAccount(datecreation);
-		Assert.assertSame(user.getDateCreateAccount(), datecreation);
-	}
-
-	@Test
-	public void getDateLastCon() {
 		user.setDateLastCon(datelogin);
-		Assert.assertSame(user.getDateLastCon(), datelogin);
-	}
-
-	@Test
-	public void getLogin() {
 		user.setLogin(login);
-		Assert.assertSame(user.getLogin(), login);
-	}
-
-	@Test
-	public void getNom() {
 		user.setNom(nom);
-		Assert.assertSame(user.getNom(), nom);
-	}
-
-	@Test
-	public void getPrenom() {
 		user.setPrenom(prenom);
-		Assert.assertSame(user.getPrenom(), prenom);
-	}
-
-	@Test
-	public void getPassword() {
 		user.setPassword(password);
-		Assert.assertSame(user.getPassword(), password);
-	}
-
-	@Test
-	public void getTelephonNumber() {
 		user.setTelephonNumber(telephone);
-		Assert.assertSame(user.getTelephonNumber(), telephone);
-	}
-
-	@Test
-	public void getEmail() {
 		user.setEmail(email);
-		Assert.assertSame(user.getEmail(), email);
-	}
-
-	@Test
-	public void getUserID() {
 		user.setUserID(userid);
+		
+		Assert.assertSame(user.getAdresse().getAdresseID(),adresseid);
+		Assert.assertSame(user.getAge(), age);
+		Assert.assertSame(user.getDateCreateAccount(), datecreation);
+		Assert.assertSame(user.getDateLastCon(), datelogin);
+		Assert.assertSame(user.getLogin(), login);
+		Assert.assertSame(user.getNom(), nom);
+		Assert.assertSame(user.getPrenom(), prenom);
+		Assert.assertSame(user.getPassword(), password);
+		Assert.assertSame(user.getTelephonNumber(), telephone);
+		Assert.assertSame(user.getEmail(), email);
 		Assert.assertSame(user.getUserID(), userid);
-
 	}
 }
