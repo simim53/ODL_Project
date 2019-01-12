28ce4bd1bdde257d55a55fb54e36f3938370b3d8
simon <simon@VMSimon.VMDOM.com>
2019-01-12 13:01:18 +0100

    update jenkins

--- ODL_REPOSITORY/odl_web/src/main/java/odl_web_controllers/UtilisateurController.java
+++ ODL_REPOSITORY/odl_web/src/main/java/odl_web_controllers/UtilisateurController.java
@@ -1,15 +1,11 @@
 package odl_web_controllers;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.douillet.odl_service_api.UtilisateurService;
-
-import odl_hibernate_model.Utilisateur;
 
 @RestController
 public class UtilisateurController {
@@ -24,13 +20,11 @@ public class UtilisateurController {
 	}
 
 	@GetMapping(path = "/getSession", headers = "Accept=*/*")
-	public String getSession(HttpSession session) {
-		 String login = (String) session.getAttribute("username");
-		 
-		 Utilisateur user = new Utilisateur();
-		 user = serviceutilisateur.findUserByUsername(login);
+	 public String currentUserNameSimple(Authentication authentication) {
+        return "null";
+    }
+		
+	 
 
-		return user.getLogin();
-	}
 
 }
